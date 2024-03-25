276eaa21e1fe95a9a174e7ebed69abb977334b2c
Taruna Makky Satya Nugraha <tarunamakki@gmail.com>
2024-03-25 13:54:08 +0700

    tugas TypeScript

---
+++ js/tugas.js
@@ -0,0 +1,18 @@
+"use strict";
+function finalScore(nama, nilai, kehadiran) {
+    if (nilai == 100 && kehadiran > 80) {
+        console.log("SELAMAT, " + nama + "! ANDA DINYATAKAN LULUS DENGAN PREDIKAT CUM LAUDE");
+    }
+    else if (nilai >= 70 && kehadiran > 70) {
+        console.log("SELAMAT, " + nama + "! ANDA DINYATAKAN LULUS");
+    }
+    else {
+        console.log("MAAF, " + nama + ", ANDA DINYATAKAN TIDAK LULUS");
+    }
+}
+let score1 = finalScore;
+score1("John", 80, 90);
+let score2 = finalScore;
+score1("Jane", 60, 65);
+let score3 = finalScore;
+score1("Alice", 100, 85);
